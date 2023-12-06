0d17e050d39fc47e9f047b4d56f8bc43088459a5
Michael Fransen <mjfransen@gmail.com>
2023-12-05 21:58:08 -0800

    Add day 6 solutions

--- src/main/kotlin/Days/Day6.kt
+++ src/main/kotlin/Days/Day6.kt
@@ -4,11 +4,39 @@ import java.io.File
 
 class Day6: Day {
     private val input = File("src/main/resources/Data/Day_6.txt").readLines()
+
+    private var regex = Regex("\\d+")
+
     override fun executePartOne() {
-        TODO("Not yet implemented")
+        val parsedData = input.map {
+            regex.findAll(it).map { result -> result.value.toLong() }
+        }
+
+        val races = (0..<parsedData[0].count()).map {
+            Race(parsedData[0].elementAt(it), parsedData[1].elementAt(it))
+        }
+
+        println( races.map { it.numberOfWaysToWin }.reduce { acc, i -> acc * i })
     }
 
     override fun executePartTwo() {
-        TODO("Not yet implemented")
+        val parsedData = input.map {
+            regex.findAll(it).map { result -> result.value }.reduce { acc, s -> acc + s }
+        }.map { it.toLong() }
+
+       println(Race(parsedData[0], parsedData[1]).numberOfWaysToWin)
     }
+}
+
+data class Race(val time: Long, val recordDistance: Long) {
+    val numberOfWaysToWin: Long
+        get() {
+            var numberOfWays = 0L
+            for (i in 1..<time) {
+                if ((time - i) * i > recordDistance) {
+                    numberOfWays++
+                }
+            }
+            return numberOfWays
+        }
 }

a6927382f5c2d40f4d48025ef0452c6cfeae8d47
Michael Fransen <mjfransen@gmail.com>
2023-12-05 18:29:39 -0800

    Add skeleton for Day 6

---
+++ src/main/kotlin/Days/Day6.kt
@@ -0,0 +1,14 @@
+package Days
+
+import java.io.File
+
+class Day6: Day {
+    private val input = File("src/main/resources/Data/Day_6.txt").readLines()
+    override fun executePartOne() {
+        TODO("Not yet implemented")
+    }
+
+    override fun executePartTwo() {
+        TODO("Not yet implemented")
+    }
+}

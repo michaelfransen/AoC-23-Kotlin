98c32aca7bb6b20f5bc2d5f9957bf38fd1c53acf
Michael Fransen <mjfransen@gmail.com>
2023-12-08 16:32:27 -0800

    Add day 8, part 2

--- src/main/kotlin/days/Day8.kt
+++ src/main/kotlin/days/Day8.kt
@@ -5,23 +5,41 @@ import java.io.File
 class Day8: Day {
     private var input = File("src/main/resources/Data/Day_8.txt").readLines()
     private val nodeRegex = Regex("([A-Z]{3}) = \\(([A-Z]{3}), ([A-Z]{3})\\)")
+    private val instructions = input[0].toCharArray()
+    private val nodes = input.subList(2, input.count())
+        .map {
+            val groups = nodeRegex.find(it)!!.groups
+            Node(groups[1]!!.value, groups[2]!!.value, groups[3]!!.value)
+        }.associateBy { it.node }
 
     override fun executePartOne() {
-        val instructions = input[0].toCharArray()
-        val nodes = input.subList(2, input.count())
-            .map {
-                val groups = nodeRegex.find(it)!!.groups
-                Node(groups[1]!!.value, groups[2]!!.value, groups[3]!!.value)
-            }.associateBy { it.node }
+        print(numberOfStepsFor(nodes["AAA"]!!) {
+            it.node == "ZZZ"
+        })
+    }
 
+    override fun executePartTwo() {
+        println(
+            nodes.filter { it.value.node.last() == 'A' }
+                .map {
+                    numberOfStepsFor(it.value) { node ->
+                        node.node.last() == 'Z'
+                    }
+                }
+                .map { it.toLong() }
+                .reduce { acc, i ->  acc.lcm(i) }
+        )
+    }
+
+    private fun numberOfStepsFor(node: Node, isFinal: (node: Node) -> Boolean): Int {
         var i = 0
         var numberOfSteps = 0
-        var node = nodes["AAA"]!!
+        var workingNode = node
 
         while (i >= 0) {
             when (instructions[i]) {
-                'L' -> node = nodes[node.leftNode]!!
-                'R' -> node = nodes[node.rightNode]!!
+                'L' -> workingNode = nodes[workingNode.leftNode]!!
+                'R' -> workingNode = nodes[workingNode.rightNode]!!
             }
 
             numberOfSteps++
@@ -31,17 +49,29 @@ class Day8: Day {
                 i++
             }
 
-            if (node.node == "ZZZ") {
+            if (isFinal(workingNode)) {
                 i = -1
             }
         }
 
-        print(numberOfSteps)
-    }
-
-    override fun executePartTwo() {
-        TODO("Not yet implemented")
+        return numberOfSteps
     }
 }
 
 data class Node(val node: String, val leftNode: String, val rightNode: String)
+
+fun Long.lcm(other: Long): Long {
+    val maxLcm = this * other
+    val larger = if (this > other) this else other
+    var lcm = larger
+
+    while (lcm <= maxLcm) {
+        if (lcm % this == 0L && lcm % other == 0L) {
+            return lcm
+        }
+
+        lcm += larger
+    }
+
+    return maxLcm
+}
